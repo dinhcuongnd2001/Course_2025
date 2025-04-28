4ede04e4970392ed1304e4f360643d3bc64b5924
rain293 <twilike9x@gmail.com>
2025-04-28 23:34:12 +0900

    feature: API post method

--- mini-project/index.js
+++ mini-project/index.js
@@ -32,3 +32,24 @@
 //     })
 //     .join("");
 // }
+
+const nameCheck = document.getElementById("name");
+const emailCheck = document.getElementById("input");
+
+const handleSubmit = async (event) => {
+  event.preventDefault();
+  if (!nameCheck.value) alert("Nhap ten");
+  if (!emailCheck.value) alert("Nhap mail");
+  // if (!emailCheck.value || !nameCheck.value) alert("Nhap day du");
+  const reposnse = await axios.post(
+    `https://dncuong.id.vn/api/users`,
+    {
+      name: nameCheck.value,
+      email: emailCheck.value,
+    },
+    {
+      headers: { "Content-Type": "application/json" },
+    }
+  );
+  console.log(reposnse);
+};
